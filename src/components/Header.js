d75453a096fa03af55c3636d4a332ec12da707ae
kmieciu6 <jakub.kmiecik96@gmail.com>
2022-06-25 23:51:53 +0200

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,25 @@ import React from 'react';
 import '../scss/main.scss';
 import {Link} from 'react-router-dom';
 import logo from '../assets/logo.png'
+import {Navbar, Nav} from "react-bootstrap";
+
 
 const Header = () => {
     return (
         <section id='header'>
             <Link to='/'><img src={logo} alt='Lady Pank' className='logo'/></Link>
-            <ul className='nav'>
-                <li><Link className='linkHeader' to='/'>Strona główna</Link></li>
-                <li><Link className='linkHeader' to='/AboutUs'>O nas</Link></li>
-                <li><Link className='linkHeader' to='/Concerts'>Koncerty</Link></li>
-                <li><Link className='linkHeader' to='/Contact'>Kontakt</Link></li>
-            </ul>
+
+            <Navbar bg="dark" expand="lg" className='navbar'>
+                <Navbar.Toggle className='toggle'/>
+                <Navbar.Collapse className='collapse'>
+                    <Nav className="me-auto nav">
+                        <Link className='linkHeader' to='/'>Strona główna</Link>
+                        <Link className='linkHeader' to='/AboutUs'>O nas</Link>
+                        <Link className='linkHeader' to='/Concerts'>Koncerty</Link>
+                        <Link className='linkHeader' to='/Contact'>Kontakt</Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </section>
     );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,6 @@ const Home = () => {
     return (
         <section id='home'>
             <Header/>
-
         </section>
     );
 }
