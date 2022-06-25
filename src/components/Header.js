72fa820548c53de8a73b3fd25239f1e502b39c38
kmieciu6 <jakub.kmiecik96@gmail.com>
2022-06-25 20:34:17 +0200

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import '../scss/main.scss'
+import '../scss/main.scss';
+import {Link} from 'react-router-dom';
+import logo from '../assets/logo.png'
 
 const Header = () => {
     return (
         <section id='header'>
-
+            <Link to='/'><img src={logo} alt='Lady Pank' className='logo'/></Link>
+            <ul className='nav'>
+                <li><Link className='linkHeader' to='/'>Strona główna</Link></li>
+                <li><Link className='linkHeader' to='/AboutUs'>O nas</Link></li>
+                <li><Link className='linkHeader' to='/Concerts'>Koncerty</Link></li>
+                <li><Link className='linkHeader' to='/Contact'>Kontakt</Link></li>
+            </ul>
         </section>
     );
 }
